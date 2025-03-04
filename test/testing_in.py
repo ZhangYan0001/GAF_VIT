cf49d5398d68b5d381096125ef43a3957a09fd02
ZhangYan <156013430@qq.com>
2025-03-04 10:21:03 +0800

    fix testing_in.py

--- test/testing_in.py
+++ test/testing_in.py
@@ -279,7 +279,7 @@ class VisionTransformer(nn.Module):
       nn.Linear(embed_dim // 2, 1)
     )
     trunc_normal_(self.pos_embed, std=0.02)
-    trunc_normal_(self.cls_token, std=0.02)
+    trunc_normal_(self.reg_token, std=0.02)
     self.apply(self._init_weights)
 
   def _init_weights(self, m):
