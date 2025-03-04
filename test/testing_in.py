857ee8544e55d0f86f0ca4221bed5b77871b01ba
ZhangYan <156013430@qq.com>
2025-03-04 10:18:47 +0800

    fix testing_in.py

--- test/testing_in.py
+++ test/testing_in.py
@@ -1,6 +1,7 @@
 import json
 import os
 import pathlib
+from functools import partial
 
 import torch
 import torch.cuda
@@ -12,7 +13,6 @@ from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from torchvision import transforms
 from tqdm import tqdm
-from functools import partial
 
 
 def to_2tuple(x):
@@ -73,34 +73,44 @@ class Attention(nn.Module):
                num_heads=8,
                qkv_bias=False,
                qk_scale=None,
-               attn_drop=0.,
+               attn_drop=0.1,
                proj_drop=0.):
     super().__init__()
     self.num_heads = num_heads
-    head_dim = dim // num_heads
-    self.scale = qk_scale or head_dim ** -0.5
+    assert dim % num_heads == 0
+    self.head_dim = dim // num_heads
+    self.scale = qk_scale or self.head_dim ** -0.5
     # 计算 q,k,v 的转移矩阵
     self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+    trunc_normal_(self.qkv.weight, std=0.02)
+    if qkv_bias:
+      nn.init.constant_(self.qkv.bias, 0)
+
     self.attn_drop = nn.Dropout(attn_drop)
     # 最终的线性层
     self.proj = nn.Linear(dim, dim)
     self.proj_drop = nn.Dropout(proj_drop)
+    # 投影层初始化
+    trunc_normal_(self.proj.weight, std=0.02)
+    nn.init.constant_(self.proj.bias, 0)
 
   def forward(self, x):
-    batch_size, N, C = x.size()
+    B, N, C = x.size()
     # 线性变换
-    qkv = self.qkv(x).reshape((batch_size, N, 3, self.num_heads, C //
-                               self.num_heads)).permute((2, 0, 3, 1, 4))
+    # qkv = self.qkv(x).reshape((batch_size, N, 3, self.num_heads, C //
+    #                            self.num_heads)).permute((2, 0, 3, 1, 4))
+    qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
     # 分割 query key value
-    q, k, v = qkv[0], qkv[1], qkv[2]
+    q, k, v = qkv.unbind(0)
     # Scaled Dot-Product Attention
     # Matmul + Scale
-    attn = (q @ k.transpose(-2, -1)) * self.scale
+    attn = torch.einsum("bhid,bhjd->bhij", q, k) * self.scale
     # SoftMax
     attn = F.softmax(attn, dim=-1)
     attn = self.attn_drop(attn)
     # Matmul
-    x = (attn @ v).transpose(1, 2).reshape(batch_size, N, C)
+    x = torch.einsum("bhid,bhjd->bhid", attn, v)
+    x = x.transpose(1, 2).reshape(B, N, C)
     # 线性变换
     x = self.proj(x)
     x = self.proj_drop(x)
@@ -113,26 +123,38 @@ class Mlp(nn.Module):
                hidden_features=None,
                out_features=None,
                act_layer=nn.GELU,
-               drop=0.):
+               drop=0.1,
+               use_ln=False
+               ):
     super().__init__()
     out_features = out_features or in_features
-    hidden_features = hidden_features or in_features
+    hidden_features = hidden_features or int(in_features * 4)
     self.fc1 = nn.Linear(in_features, hidden_features)
     self.act = act_layer()
+    self.drop1 = nn.Dropout(drop)
     self.fc2 = nn.Linear(hidden_features, out_features)
-    self.drop = nn.Dropout(drop)
+    self.drop2 = nn.Dropout(drop)
+    # 可选层归一化
+    self.norm = nn.LayerNorm(hidden_features) if use_ln else nn.Identity()
+    # 初始化
+    trunc_normal_(self.fc1.weight, std=0.02)
+    nn.init.constant_(self.fc1.bias, 0)
+    trunc_normal_(self.fc2.weight, std=0.02)
+    nn.init.constant_(self.fc2.bias, 0)
 
   def forward(self, x):
     # 输入层：线性变换
     x = self.fc1(x)
     # 应用激活函数
     x = self.act(x)
+    # 可选层归一化
+    x = self.norm(x)
     # Dropout
-    x = self.drop(x)
+    x = self.drop1(x)
     # 输出层：线性变换
     x = self.fc2(x)
     # Dropout
-    x = self.drop(x)
+    x = self.drop2(x)
     return x
 
 
@@ -170,10 +192,10 @@ class Block(nn.Module):
                attn_drop=0.,
                drop_path=0.,
                act_layer=nn.GELU,
-               norm_layer="nn.LayerNorm",
-               epsilon=1e-5):
+               norm_layer=partial(nn.LayerNorm, eps=1e-5),
+               ):
     super().__init__()
-    self.norm1 = eval(norm_layer)(dim, eps=epsilon)
+    self.norm1 = norm_layer(dim)
     # Multi-head self-attention
     self.attn = Attention(
       dim,
@@ -185,7 +207,7 @@ class Block(nn.Module):
     )
     # DropPath
     self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-    self.norm2 = eval(norm_layer)(dim, eps=epsilon)
+    self.norm2 = norm_layer(dim)
     mlp_hidden_dim = int(dim * mlp_ratio)
     self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
@@ -212,7 +234,7 @@ class VisionTransformer(nn.Module):
     drop_rate=0.,
     attn_drop_rate=0.,
     drop_path_rate=0.,
-    norm_layer = partial(nn.LayerNorm, eps=1e-5),
+    norm_layer=partial(nn.LayerNorm, eps=1e-5),
     **kwargs
   ):
     super().__init__()
@@ -252,9 +274,9 @@ class VisionTransformer(nn.Module):
     ])
     self.norm = norm_layer(embed_dim)
     self.reg_head = nn.Sequential(
-      nn.Linear(embed_dim, embed_dim//2),
+      nn.Linear(embed_dim, embed_dim // 2),
       nn.GELU(),
-      nn.Linear(embed_dim//2, 1)
+      nn.Linear(embed_dim // 2, 1)
     )
     trunc_normal_(self.pos_embed, std=0.02)
     trunc_normal_(self.cls_token, std=0.02)
@@ -326,7 +348,7 @@ def get_soh_dict(file_path: str):
   return dict(SOH_Labels)
 
 
-def get_images_path( image_dir:str, image_keys: [str]):
+def get_images_path(image_dir: str, image_keys: [str]):
   if not os.path.exists(image_dir):
     print("the image_dir is not exist,please input a new path")
   images = []
@@ -409,7 +431,7 @@ def create_loaders(batch_size=32):
 
   # 按你的划分策略分离数据
   train_paths = get_images_path(image_path, train_image_keys)
-  val_paths = get_images_path(image_path,val_image_keys )
+  val_paths = get_images_path(image_path, val_image_keys)
   test_paths = get_images_path(image_path, test_image_keys)
 
   # 获取对应的标签切片
